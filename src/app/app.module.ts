11b9aa60677880c17e6253d54ee84b68726ce637
Khgds1 <khgds1@hotmail.com>
2020-02-29 07:49:58 -0300

    adicionando novas páginas ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AmiicComponent } from './pages/projects/amiic/amiic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    AmiicComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/projects/amiic/amiic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amiic',
+  templateUrl: './amiic.component.html',
+  styleUrls: ['./amiic.component.css']
+})
+export class AmiicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
