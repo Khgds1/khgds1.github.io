542a17a1a8389312656614e9b8b3dacf7e1258da
Khgds1 <khgds1@hotmail.com>
2020-02-29 15:30:38 -0300

    modificando diversas coisas no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +15,7 @@ import { AmiicComponent } from './pages/projects/amiic/amiic.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SupportComponent } from './pages/support/support.component';
 import { SocialComponent } from './pages/social/social.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +29,12 @@ import { SocialComponent } from './pages/social/social.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
