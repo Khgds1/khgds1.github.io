8afde002ec968e9d18c4ca18fa3847505e2fc197
Khgds1 <khgds1@hotmail.com>
2020-02-29 11:00:47 -0300

    adicionando novas paginas e configurando novos caminhos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent} from './pages/home/home.component';
+import { ProjectsComponent} from './pages/projects/projects.component';
+import { AmiicComponent} from './pages/projects/amiic/amiic.component';
+import { AboutComponent} from './pages/about/about.component';
+import { SupportComponent} from './pages/support/support.component';
+import {SocialComponent} from './pages/social/social.component'
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'projetos', component: ProjectsComponent},
+  { path: 'amiic', component: AmiicComponent},
+  { path: 'sobre', component: AboutComponent},
+  { path: 'donate', component: SupportComponent},
+  { path: 'social', component: SocialComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { AmiicComponent } from './pages/projects/amiic/amiic.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SupportComponent } from './pages/support/support.component';
+import { SocialComponent } from './pages/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProjectsComponent,
-    AmiicComponent
+    AmiicComponent,
+    AboutComponent,
+    SupportComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
